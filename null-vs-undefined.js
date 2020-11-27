e52d104a5583330f4f5b4ab2c750ba38d5a057cf
nafiul-shishir <nafiul.shishir@gmail.com>
2020-11-27 20:44:00 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,15 @@
+// let pakhi;
+
+// function add (num1,num2)
+// {
+//     console.log(num1+num2);
+//     return 
+// }
+
+// function add(num1,num2){
+//     console.log(num1,num2)
+// }
+
+
+const permik = {name: "smart dude", phone:"01687110333"}
+console.log(permik.gf);
